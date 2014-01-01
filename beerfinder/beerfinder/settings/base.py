b7059f9e9b47e96b8024790d72692e97b02bcee3
Justin Michalicek <jmichalicek@gmail.com>
2014-01-01 18:38:27 +0000

    Added routing to django.contrib.auth.urls and TEMPLATE_DIRS setting

--- beerfinder/beerfinder/settings/base.py
+++ beerfinder/beerfinder/settings/base.py
@@ -113,3 +113,10 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'),
+)
--- beerfinder/beerfinder/urls.py
+++ beerfinder/beerfinder/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'api/', include('beer.routers')),
                        url(r'api/', include('sighting.routers')),
+                       url(r'accounts/', include('django.contrib.auth.urls')),
 
 )
