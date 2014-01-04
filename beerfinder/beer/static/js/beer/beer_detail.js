6cf019e152a75e7770fc7179ea3c3ae00366a626
Justin Michalicek <jmichalicek@gmail.com>
2014-01-04 14:28:05 +0000

    beer detail page

--- beerfinder/beer/static/js/beer/beer_detail.js
+++ beerfinder/beer/static/js/beer/beer_detail.js
@@ -2,5 +2,16 @@ var ViewModel = function (data) {
     var self = this;
 
     this.beer = ko.observable(new BeerModel(data.beer));
+    this.sightings = ko.observableArray();
 
+    this.getSightings = function () {
+        $.ajax({url: '/api/sightings/',
+                method: 'GET',
+                data: {beer_slug: self.beer().slug()}
+               }).done(function (data) {
+                   ko.utils.arrayForEach(data, function(item) {
+                       self.sightings.push(new SightingModel(item));
+                   });
+               });
+    };
 };
