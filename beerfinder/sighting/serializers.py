e20257a7d7925a31bc7619834715fc026a5a230c
Justin Michalicek <jmichalicek@gmail.com>
2014-01-04 14:27:30 +0000

    Sighting serializer now uses VenueSerializer() for the venue related field

--- beerfinder/sighting/serializers.py
+++ beerfinder/sighting/serializers.py
@@ -2,13 +2,14 @@ from django.forms import widgets
 from rest_framework import serializers
 
 from beer.serializers import BeerSerializer
+from venue.serializers import VenueSerializer
 
 from .models import Sighting
 
 class SightingSerializer(serializers.HyperlinkedModelSerializer):
     sighted_by = serializers.Field()
     beer = BeerSerializer()
-    venue = serializers.RelatedField() # TODO: write a VenueSerializer
+    venue = VenueSerializer()
 
     class Meta:
         model = Sighting
