23556a78bfcc782d5614c60ea4d9040d40835f51
Justin Michalicek <jmichalicek@gmail.com>
2014-01-26 04:10:47 +0000

    edit profile view now request correct data from api and fills form with initial values

--- beerfinder/accounts/static/js/accounts/edit_profile.js
+++ beerfinder/accounts/static/js/accounts/edit_profile.js
@@ -1,4 +1,19 @@
 var EditProfileViewModel = function (data) {
     var self = this;
 
+    this.profile = ko.observable(new MyAccountModel({}));
+
+    this.updateProfile = function () {
+    };
+
+    this.getProfile = function () {
+        $.ajax({
+            url: '/api/profile/me/',
+            type: 'GET'
+        }).done(function (data) {
+            self.profile(new MyAccountModel(data));
+        });
+    }
+
+    self.getProfile();
 };
--- beerfinder/accounts/static/js/accounts/models.js
+++ beerfinder/accounts/static/js/accounts/models.js
@@ -3,5 +3,8 @@ var MyAccountModel = function (data) {
 
     self.firstName = ko.observable(data.first_name);
     self.lastName = ko.observable(data.last_name);
+    self.email = ko.observable(data.email);
+    self.username = ko.observable(data.username);
+    self.showNameOnSightings =  ko.observable(data.show_name_on_sightings);
     
 }
