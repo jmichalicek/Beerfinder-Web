bc6abb45a739de3b1e7afd84924f8ac2bf794475
Justin Michalicek <jmichalicek@gmail.com>
2014-02-16 22:12:56 +0000

    edited temporary alert popup for adding beer to watchlist

--- beerfinder/beer/static/js/beer/beer_detail.js
+++ beerfinder/beer/static/js/beer/beer_detail.js
@@ -15,13 +15,15 @@ var ViewModel = function (data) {
                });
     };
 
+    // TODO: Maybe add something to api which shows whether beer is in the user's watchlist or not
+    // and have remove from watchlist if it is already there and change that after adding
     this.addToWatchlist = function (data) {
         $.ajax({
             url: '/api/watchlist/',
             type: 'POST',
             data: {'beer': self.beer().slug()}
         }).done(function (data) {
-            alert("Added to watchlist " + data);
+            alert("Added to watchlist");
         }).fail(function (data) {
             console.log("error adding beer to watchlist: " + data);
         });
