a4be1803fcbede590eadf4191e22d06d06255d9a
Justin Michalicek <jmichalicek@gmail.com>
2014-05-22 02:59:40 +0000

    fixed js fallback image loading so that it does not blow up when there is no image on the sighting

--- beerfinder/sighting/static/js/sighting/viewmodels/SightingDetailViewModel.js
+++ beerfinder/sighting/static/js/sighting/viewmodels/SightingDetailViewModel.js
@@ -52,12 +52,16 @@ define(['jquery', 'knockout', 'sighting/models/SightingModel', 'sighting/models/
 
         this.imageUrl = ko.computed(function () {
             var image = self.sighting().primaryImage();
-
+            if(!image) {
+                return '';
+            }
             // not really the right way to do this fallback with knockout, but it works
             var pic = document.getElementById('sighting_image');
-            pic.onerror = function () {
-                pic.src = image.originalUrl();
-            };
+            if(pic) {
+                pic.onerror = function () {
+                    pic.src = image.originalUrl();
+                };
+            }
             return image.mediumUrl() ? image.mediumUrl() : image.originalUrl();
         });
     };
