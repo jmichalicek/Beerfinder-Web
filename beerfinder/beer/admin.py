e6e3a447d5ce95cf3db767f2ebc0efff064de293
Justin Michalicek <jmichalicek@gmail.com>
2014-06-07 03:39:44 +0000

    Added some filters, search fields, and list display fields for beer in django admin

--- beerfinder/beer/admin.py
+++ beerfinder/beer/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Beer, Brewery, ServingType, Style
 
-admin.site.register(Beer)
+class BeerAdmin(admin.ModelAdmin):
+    list_filter = ('style', )
+    list_display = ('name', 'brewery', 'date_created')
+    search_fields = ('style__name', 'brewery__name', 'name')
+
+admin.site.register(Beer, BeerAdmin)
 admin.site.register(Brewery)
 admin.site.register(ServingType)
 admin.site.register(Style)
