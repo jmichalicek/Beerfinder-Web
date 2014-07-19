14d09fc7e1fc8c26d590c117b83a8dbc1a5ce8ce
Justin Michalicek <jmichalicek@gmail.com>
2014-07-19 02:44:40 +0000

    navbar update location link rotates when location is updating.  Listens for other viewmodels to publish geolocation start and success messages.

--- beerfinder/beerfinder/static/js/core/viewmodels/NavBarViewModel.js
+++ beerfinder/beerfinder/static/js/core/viewmodels/NavBarViewModel.js
@@ -4,17 +4,33 @@ define(['knockout', 'pubsub', 'core/PubSubChannels'], function(ko, PubSub, PubSu
         var self = this;
 
         this.location = ko.observable();
+        this.locationUpdating = ko.observable(false);
         this.updateLocation = function () {
+            self.locationUpdating(true);  // viewModel does not listen to it's own sent messages
             PubSub.publish(PubSubChannels.GEOLOCATION_START, {});
             navigator.geolocation.getCurrentPosition(self.locationSuccess, self.locationError);
+            $('#nav-update-location').blur();
         };
 
         this.locationSuccess = function (position) {
+            self.locationUpdating(false);
             PubSub.publish(PubSubChannels.GEOLOCATION_SUCCESS, position);
         };
 
         this.locationError = function () {
+            self.locationUpdating(false);
         };
+
+        self.geoLocationStartMessageHandler = function (msg, data) {
+            self.locationUpdating(true);
+        };
+
+        self.geoLocationSuccessMessageHandler = function (msg, data) {
+            self.locationUpdating(false);
+        };
+
+        PubSub.subscribe(PubSubChannels.GEOLOCATION_SUCCESS, self.getLocationSuccessMessageHandler);
+        PubSub.subscribe(PubSubChannels.GEOLOCATION_START, self.getLocationStartMessageHandler);
 
     };
 });
