06278570603d61fdebdd10f4ecdd5406fc034377
Justin Michalicek <jmichalicek@gmail.com>
2014-08-15 01:30:25 +0000

    properly configured compass

--- beerfinder/beerfinder/static/compass/config.rb
+++ beerfinder/beerfinder/static/compass/config.rb
@@ -8,6 +8,7 @@ sass_dir = "sass"
 images_dir = "../img"
 javascripts_dir = "../js/vendor"
 fonts_dir = "../fonts"
+relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
