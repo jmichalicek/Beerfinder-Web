a45808fa9a18f67ec92a0c8a699bcc6f23974b95
Justin Michalicek <jmichalicek@gmail.com>
2014-11-25 02:54:49 +0000

    Added constants for geolocation error messages

--- beerfinder/beerfinder/static/js/core/Constants.js
+++ beerfinder/beerfinder/static/js/core/Constants.js
@@ -3,5 +3,8 @@ define([], function() {
     constants.GEOLOCATION_FAIL_DENIED = 1;
     constants.GEOLOCATION_FAIL_UNAVAILABLE = 2;
     constants.GEOLOCATION_FAIL_TIMEOUT = 3;
+    constants.GEOLOCATION_DENIED_MESSAGE = "You have declined access to your location.  Some features will not work.";
+    constants.GEOLOCATION_UNAVAILABLE_MESSAGE = "We were unable to determine your location.  Some features will not work.";
+    constants.GEOLOCATION_TIMEOUT_MESSAGE = constants.GEOLOCATION_UNAVAILABLE_MESSAGE;
     return Object.freeze(constants);
 });
