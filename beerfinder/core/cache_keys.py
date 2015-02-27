20e9e71b165af32d4b25c0ecd68c712b19372566
Justin Michalicek <jmichalicek@gmail.com>
2015-02-27 03:19:17 +0000

    added improved QueryParamsKeyConstructor

--- beerfinder/core/cache_keys.py
+++ beerfinder/core/cache_keys.py
@@ -1,5 +1,7 @@
 from rest_framework_extensions.key_constructor.constructors import DefaultKeyConstructor
 from rest_framework_extensions.key_constructor.bits import (
+    ArgsKeyBit,
+    KwargsKeyBit,
     KeyBitBase,
     RetrieveSqlQueryKeyBit,
     ListSqlQueryKeyBit,
@@ -7,6 +9,10 @@ from rest_framework_extensions.key_constructor.bits import (
     UserKeyBit,
     QueryParamsKeyBit
 )
+
+
+class QueryParamsKeyConstructor(DefaultKeyConstructor):
+    query_params = QueryParamsKeyBit('*')
 
 
 class DefaultPaginatedListKeyConstructor(DefaultKeyConstructor):
